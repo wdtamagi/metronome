4da510f74533481001f202b803d971fb9c97863c
Willian Dias Tamagi <will.tamagi@gmail.com>
2021-03-31 23:24:23 -0300

    Add stick animation with react spring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
+import { useSpring, animated } from "react-spring";
 import "./App.css";
 
-function App() {
+const duration = 900;
+
+const App: FC = () => {
+  const [beep, setBeep] = useState(true);
+  useEffect(() => {
+    const interval = setInterval(() => setBeep((s) => !s), duration);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  const props = useSpring({
+    transform: `rotate(${beep ? "-" : ""}55deg)`,
+    config: { duration },
+  });
   return (
     <div className="App">
-      <div className="Stick"></div>
+      <animated.div style={props} className="Stick"></animated.div>
     </div>
   );
-}
+};
 
 export default App;
